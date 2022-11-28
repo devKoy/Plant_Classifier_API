c8a95a4d5d81cdb3c073260b25b9adc9a39fc46e
Lloyd <83859566+devKoy@users.noreply.github.com>
2022-11-28 11:34:20 +0800

    fixing CORS

--- server.py
+++ server.py
@@ -7,8 +7,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 origins = [
-	"https://www.greenlyai.space/",
-	"https://greenlyai.space/",
 	"https://www.greenlyai.space",
 	"https://greenlyai.space"
 ]
@@ -16,7 +14,8 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins = origins,
-    allow_methods=["POST"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
