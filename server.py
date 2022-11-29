fc1c28e93a090262264336599ea1c504b799a492
Lloyd <83859566+devKoy@users.noreply.github.com>
2022-11-29 08:34:08 +0800

    Update server.py

--- server.py
+++ server.py
@@ -7,20 +7,6 @@ from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from fastapi.routing import APIRoute
 
-
-@app.get('/main')
-async def hello():
-	return "HELLO"
-    
-@app.post('/predict')
-async def predict_api(file : UploadFile = None):
-	
-	extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
-	if not extension:
-		return "Image must be a [jpg, jpeg, png] format"
-	image = r.read_files(await file.read())
-	pred = p.predict(image)
-	return pred
 origins = [
    "https://testapi.consulting.repl.co",
    "https://www.greenlyai.space",
@@ -53,6 +39,20 @@ async def add_CORS_header(request: Request, call_next):
     response.headers['Access-Control-Allow-Headers'] = '*'
     return response
 
+
+@app.get('/main')
+async def hello():
+	return "HELLO"
+    
+@app.post('/predict')
+async def predict_api(file : UploadFile = None):
+	
+	extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+	if not extension:
+		return "Image must be a [jpg, jpeg, png] format"
+	image = r.read_files(await file.read())
+	pred = p.predict(image)
+	return pred
 
 if __name__ == "__main__":
 	uvicorn.run(app, host='0.0.0.0', port=os.environ.get('PORT', '5000'))
