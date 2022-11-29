438f017870ce62c972cc6674fbcd1dd89c5aef21
Lloyd <83859566+devKoy@users.noreply.github.com>
2022-11-29 08:27:34 +0800

    Cors test: adding apiroute

--- server.py
+++ server.py
@@ -1,22 +1,11 @@
 from distutils.log import debug
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, Form, Body, Request, Response
 import uvicorn
 from application import prediction as p, read_files as r
 import os
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
-
-origins = [
-   "https://testapi.consulting.repl.co",
-   "https://www.greenlyai.space",
-   "https://greenlyai.space"
-]
-
-middleware = [
-    Middleware(CORSMiddleware, allow_origins=origins,  allow_headers=["*"])
-]
-
-app = FastAPI(middleware=middleware)
+from fastapi.routing import APIRoute
 
 
 @app.get('/main')
@@ -32,6 +21,38 @@ async def predict_api(file : UploadFile = None):
 	image = r.read_files(await file.read())
 	pred = p.predict(image)
 	return pred
+origins = [
+   "https://testapi.consulting.repl.co",
+   "https://www.greenlyai.space",
+   "https://greenlyai.space"
+]
+
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=origins,  allow_headers=["*"])
+]
+
+app = FastAPI(middleware=middleware)
+
+ALLOWED_ORIGINS = '*' 
+
+# handle CORS preflight requests
+@app.options('/*')
+async def preflight_handler(request: Request, rest_of_path: str) -> Response:
+    response = Response()
+    response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    return response
+
+# set CORS headers
+@app.middleware("http")
+async def add_CORS_header(request: Request, call_next):
+    response = await call_next(request)
+    response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    return response
+
 
 if __name__ == "__main__":
 	uvicorn.run(app, host='0.0.0.0', port=os.environ.get('PORT', '5000'))
